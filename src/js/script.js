bc9cbd5e2564b7de1336f89a3fb700ef2ff80bf5
Vladimir <nevajnooru@gmail.com>
2020-11-18 21:48:55 +0300

    links js and buttons img

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('.carousel__inner').slick({
   speed: 1200,
   //adaptiveHeight: true,
-  prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.png"></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="../img/nextpng.png"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png"></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/nextpng.png"></button>',
   responsive: [
     {
       breakpoint: 992,
